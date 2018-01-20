4253b6b420aee37d9fbc8af5f1b4167b7a4a99ab
Łukasz Jurczyk <admin@zabszk.net>
2018-01-20 15:27:40 +0100

    Event fixed.

--- src/net/zabszk/networklogin/Event.java
+++ src/net/zabszk/networklogin/Event.java
@@ -11,6 +11,7 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Event implements Listener {
@@ -42,18 +43,31 @@ public class Event implements Listener {
             command = command.substring(1);
             if (command.contains(" ")) command = command.substring(0, command.indexOf(" "));
         }
-        String[] args;
-        if (e.getMessage().contains(" ")) args = e.getMessage().substring(command.length() + 1).split(" ");
-        else args = new String[0];
+        List<String> args = new ArrayList(Arrays.asList(e.getMessage().split(" ")));
+        args.remove(0);
 
         if (command.equalsIgnoreCase("login") || command.equalsIgnoreCase("l") || command.equalsIgnoreCase("signin")) {
             e.setCancelled(true);
-            if (args.length == 1) {
+            if (args.size() == 2 && args.get(0).equalsIgnoreCase("-f")) {
+                if (e.getPlayer().hasPermission("networklogin.forcelogin") && e.getPlayer().isOp() && Main.getInstance().IsAuthenticated(e.getPlayer(), true)) {
+                    try {
+                        Player tr = Bukkit.getPlayer(args.get(1));
+                        if (tr == null || !tr.isOnline()) {
+                            e.getPlayer().sendMessage(ChatColor.RED + "[Zabszk NetworkLogin] Player " + args.get(1) + " is offline.");
+                            return;
+                        }
+                        System.out.println("[Zabszk NetworkLogin] Player " + tr.getName() + " has been forced authenticated by " + e.getPlayer().getName());
+                        Main.getInstance().SetAuthenticated(tr, true);
+                    } catch (Exception ex) {
+                        e.getPlayer().sendMessage(ChatColor.RED + "[Zabszk NetworkLogin] Player " + args.get(1) + " is offline or error occurred.");
+                    }
+                } else
+                    e.getPlayer().sendMessage(ChatColor.RED + "[Zabszk NetworkLogin] You don't have permissions to force authenticate.");
+            } else if (args.size() > 0) {
                 if (Main.getInstance().IsAuthenticated(e.getPlayer(), false))
                     e.getPlayer().sendMessage(Functions.getMessage("AlreadyLogged"));
                 System.out.println("[Zabszk NetworkLogin] Player " + e.getPlayer().getName() + " is trying to authenticate...");
-
-                int response = Functions.Authenticate(e.getPlayer().getName(), args[0]);
+                int response = Functions.Authenticate(e.getPlayer().getName(), args.get(0));
                 System.out.println("[Zabszk NetworkLogin] Authentication result of player " + e.getPlayer().getName() + " is " + response);
 
                 if (response == 0) e.getPlayer().sendMessage(Functions.getMessage("AccountNotFoundMessage"));
@@ -62,21 +76,6 @@ public class Event implements Listener {
                 else if (response == 3) e.getPlayer().sendMessage(Functions.getMessage("AccountInactiveMessage"));
                 else if (response == 4) e.getPlayer().sendMessage(Functions.getMessage("AccountBannedMessage"));
                 else e.getPlayer().sendMessage(Functions.getMessage("InternalErrorMessage"));
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("-f")) {
-                if (e.getPlayer().hasPermission("networklogin.forcelogin") && e.getPlayer().isOp() && Main.getInstance().IsAuthenticated(e.getPlayer(), true)) {
-                    try {
-                        Player tr = Bukkit.getPlayer(args[1]);
-                        if (tr == null || !tr.isOnline()) {
-                            e.getPlayer().sendMessage(ChatColor.RED + "[Zabszk NetworkLogin] Player " + args[1] + " is offline.");
-                            return;
-                        }
-                        System.out.println("[Zabszk NetworkLogin] Player " + tr.getName() + " has been forced authenticated by " + e.getPlayer().getName());
-                        Main.getInstance().SetAuthenticated(tr, true);
-                    } catch (Exception ex) {
-                        e.getPlayer().sendMessage(ChatColor.RED + "[Zabszk NetworkLogin] Player " + args[1] + " is offline or error occurred.");
-                    }
-                } else
-                    e.getPlayer().sendMessage(ChatColor.RED + "[Zabszk NetworkLogin] You don't have permissions to force authenticate.");
             } else e.getPlayer().sendMessage(ChatColor.RED + "Syntax: /login password OR /login -f username");
         } else {
             if (!Main.getInstance().IsAuthenticated(e.getPlayer(), false)) {
